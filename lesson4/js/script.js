359057949903f4cbf33575294f3c69a8395b4e76
TLatu2020 <tlatu@byui.edu>
2020-10-08 18:46:45 +1100

    code added for responsive menu

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,7 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
 document.getElementById("lastupdated").innerHTML = document.lastModified;
 
 document.getElementById("currentyear").textContent = new Date().getFullYear();
