bf39e194fa1894438b61dad9dc1a1283bd4c8173
TLatu2020 <tlatu@byui.edu>
2020-10-16 17:35:13 +1100

    code improved

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -2,6 +2,18 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
+function showBanner() {
+    let day = new Date()
+    const banner = document.getElementsByClassName('pancake-banner');
+
+    if (day.getDay() == 4) {
+        banner.style.display = 'block';
+    } else {
+        banner.style.display = 'none';
+    }
+
+}
+
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
