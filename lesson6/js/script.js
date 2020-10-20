a4f1c6597379a24c4994df48a5b79d51a45112fe
TLatu2020 <tlatu@byui.edu>
2020-10-20 19:32:00 +1100

    JavaScript added and Font loaded

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -17,3 +17,11 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+WebFont.load({
+    google: {
+        families: [
+            'Roboto Slab', 'Montserrat'
+        ]
+    }
+});
