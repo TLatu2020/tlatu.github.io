f83f54233362fac5eccc73e771916a5669735dbe
TLatu2020 <tlatu@byui.edu>
2020-10-26 10:55:55 +1100

    gallery page files added

---
+++ lesson7/js/script.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+function showBanner() {
+    let d = new Date()
+    const banner = document.getElementById('pancake-banner');
+
+    if (d.getDay() == 5) {
+        banner.style.display = "block";
+    } else {
+        banner.style.display = "none";
+    }
+}
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+WebFont.load({
+    google: {
+        families: [
+            'Roboto Slab', 'Montserrat'
+        ]
+    }
+});
