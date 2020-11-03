7e7f3d208c90e67f0ada0621e3fbf9c21aa6f421
TLatu2020 <tlatu@byui.edu>
2020-11-03 17:17:34 +1100

    code improved

---
+++ lesson8/js/script.js
@@ -0,0 +1,29 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+function showBanner() {
+    let d = new Date()
+    const banner = document.getElementById('pancake-banner');
+
+    if (d.getDay() == 5) {
+        banner.style.display = "block";
+    }
+}
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+WebFont.load({
+    google: {
+        families: [
+            'Roboto Slab', 'Montserrat'
+        ]
+    }
+});
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
