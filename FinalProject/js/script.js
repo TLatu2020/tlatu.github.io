0443b1809be07f904293e8cfd9a5fba466384a53
TLatu2020 <tlatu@byui.edu>
2020-12-04 15:24:40 +1100

    code improved

--- FinalProject/js/script.js
+++ FinalProject/js/script.js
@@ -20,3 +20,19 @@ function showSlides(n) {
 
     slides[slideIndex - 1].style.display = "block";
 }
+
+//Current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//Current date
+const options = { weekday = 'long', day: 'numeric', month: 'long', year: 'numeric' };
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+//Fonts
+WebFont.load({
+    google: {
+        families: [
+            'Roboto Slab', 'Montserrat'
+        ]
+    }
+});
