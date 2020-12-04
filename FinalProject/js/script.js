9519267cd8324c4d58e5a7f91a30fc6434d49f72
TLatu2020 <tlatu@byui.edu>
2020-12-04 15:42:01 +1100

    code improved

--- FinalProject/js/script.js
+++ FinalProject/js/script.js
@@ -25,7 +25,7 @@ function showSlides(n) {
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 //Current date
-const options = { weekday = 'long', day: 'numeric', month: 'long', year: 'numeric' };
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
 //Fonts
