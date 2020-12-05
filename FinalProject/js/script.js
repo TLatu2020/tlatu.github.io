4a497a8a19f6eabb4fd19063b5d4ce55ad32eee6
TLatu2020 <tlatu@byui.edu>
2020-12-05 17:23:57 +1100

    code improved

--- FinalProject/js/script.js
+++ FinalProject/js/script.js
@@ -1,26 +1,3 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var slides = document.getElementsByClassName("slides");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-
-    for (var i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-}
-
 //Current year
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
