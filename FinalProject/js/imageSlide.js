01143e9ed0837ffcc7bed733068211c7e0bd4c45
TLatu2020 <tlatu@byui.edu>
2020-12-17 17:13:22 +1100

    Code improved

--- FinalProject/js/imageSlide.js
+++ FinalProject/js/imageSlide.js
@@ -20,3 +20,18 @@ function showSlides(n) {
 
     slides[slideIndex - 1].style.display = "block";
 }
+
+var autoSlides = 0;
+autoShow();
+
+function autoShow() {
+    var i;
+    var slides = document.getElementsByClassName("myAutos");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    autoSlides++;
+    if (autoSlides > slides.length) { autoSlides = 1 }
+    slides[autoSlides - 1].style.display = "block";
+    setTimeout(autoShow, 4000);
+}
